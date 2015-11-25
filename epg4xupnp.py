567d61782d20d2fa8fd2e1f0db55d021788487d0
Antonio Monteagudo Chaves <themonti@iMac-de-Antonio.local>
2015-11-25 01:00:23 +0100

    Añadido epg2.m3u para servir desde la base de datos

--- epg4xupnp.py
+++ epg4xupnp.py
@@ -188,17 +188,17 @@ http://server4.balkantvmedia.com:8080/XXX2?u=user173:p=7wdjPw59
 http://server4.balkantvmedia.com:8080/XXX3?u=user173:p=7wdjPw59
 #EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] PASSION XXX
 http://q.gs/9830047/sex3todokodi
-#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar,[ADULT] Xtsy XXX
+#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] Xtsy XXX
 http://public.tuiptv.biz/public/PlayList.php?id=20e12547-5adb-4dc6-89af-41e6e7e6d4f3&channel=31
-#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar,[ADULT] uicy XXX
+#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] uicy XXX
 http://public.tuiptv.biz/public/PlayList.php?id=20e12547-5adb-4dc6-89af-41e6e7e6d4f3&channel=30
-#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar,[ADULT] Real XXX
+#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] Real XXX
 http://public.tuiptv.biz/public/PlayList.php?id=20e12547-5adb-4dc6-89af-41e6e7e6d4f3&channel=29
-#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar,[ADULT] Sextreme XXX
+#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] Sextreme XXX
 http://public.tuiptv.biz/public/PlayList.php?id=20e12547-5adb-4dc6-89af-41e6e7e6d4f3&channel=26
-#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar,[ADULT] Venus XXX
+#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] Venus XXX
 http://public.tuiptv.biz/public/PlayList.php?id=20e12547-5adb-4dc6-89af-41e6e7e6d4f3&channel=25
-#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar,[ADULT] Playboy TV Latin America
+#EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] Playboy TV Latin America
 http://public.tuiptv.biz/public/PlayList.php?id=20e12547-5adb-4dc6-89af-41e6e7e6d4f3&channel=28
 #EXTINF:-1 type=mpeg dlna_extras=mpeg_ps_pal group-title="NewMovistar",[ADULT] HUSTLER HD
 http://bit.ly/1jGYwsX
